feefc6ec01738a7887229d95bcb57850cbc88530
kwilson2004 <k_w2004@student.phsc.edu>
2023-01-09 20:05:10 -0500

    #new projects and updates to files

---
+++ Chapter 3 /prjs3_2.js
@@ -0,0 +1,10 @@
+var myheading = "<h1>This is My Web Page!</h1>",
+linktag = "<a href=\"http://wwww.webxpertz.net\">WebSiteLink!</a>",
+sometext = "This text can be affected by other statments.",
+begineffect = "<strong>",
+endeffect = "</strong>",
+beginpara = "<p>",
+endpara = "</p>";
+document.write(myheading + begineffect + sometext + endeffect + beginpara + linktag + endpara + beginpara + sometext +  endpara);
+
+
---
+++ Submited projects/Project3/project3.js
@@ -0,0 +1,15 @@
+// This an example of a text/string variable.
+var stext = "this is a short text description of a string variable.";
+//This is a example of a number variable.
+var milk = 10;
+var tax = .5;
+//this is a string variable that ads a break in the html file to seperate the first string form the total.
+var linebr = "<br>";
+//This line uses template literals to divide the milk and tax variable.
+let total = `Total: ${(milk / tax)}`;
+
+//This displays all the code to the html file.
+document.write(stext + linebr +  total);
+
+//This changes the background color to red.
+document.body.style.backgroundColor = "red";

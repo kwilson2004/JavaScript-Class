1d82f4ebe70e44b749477c3a62fe5254224362b7
kwilson2004 <k_w2004@student.phsc.edu>
2023-01-12 11:58:19 -0500

    update

---
+++ Chapter 5/prjs5_1.js
@@ -0,0 +1,12 @@
+var paycheck = 2000;
+
+paycheck += 2000;
+document.write(paycheck + "<br>");
+paycheck -= 500;
+document.write(paycheck + "<br>");
+paycheck *= 0;
+document.write(paycheck + "<br>");
+paycheck += 500;
+document.write(paycheck + "<br>");
+paycheck -= 80;
+document.write(paycheck + "<br>");
